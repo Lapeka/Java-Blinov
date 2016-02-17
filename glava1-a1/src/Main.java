ee9b8a2eb42fa8d42eca805ef8e83239998b248f
Lapeka <rembo_vs_terminator@mail.ru>
2016-02-17 23:42:56 +0300

    update

---
+++ glava1-a1/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Press enter you name: ");
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Hello " + scn.next());
+    }
+}
